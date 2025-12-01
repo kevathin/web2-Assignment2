ab2cf286548ab11c74ee2a8a21ed9c347b6342ee
kevathin <Kbert395@mtroyal.ca>
2025-12-01 14:33:34 -0700

    about 30-35% done

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", (e)=>{
 
     /*  
         a list containing all the filters that the user wanted
-        "N/A" means no filter for that category.
+        "N/A" means no filter for that filter.
         orderBy: LtoH (low to high cost), HtoL (high to low cost)
     */
     let filterList = {
@@ -27,8 +27,10 @@ document.addEventListener("DOMContentLoaded", (e)=>{
         "sizeM":"N/A",
         "sizeL":"N/A",
         "sizeXL":"N/A",
-        "orderBy":"N/A"
+        "sortBy":"N/A"
     };
+
+    // ------------------------------------------ fetch + setup manager----------------
 
     fetch("https://gist.githubusercontent.com/rconnolly/d37a491b50203d66d043c26f33dbd798/raw/37b5b68c527ddbe824eaed12073d266d5455432a/clothing-compact.json")
         .then(Response =>{
@@ -41,7 +43,7 @@ document.addEventListener("DOMContentLoaded", (e)=>{
             data = info;
             pageManager();
         }).catch(err =>{
-            
+            alert("fetch failure");
         });
     
     /**
@@ -63,15 +65,16 @@ document.addEventListener("DOMContentLoaded", (e)=>{
      * instead of html.
      */
     function initialPageHidden(){
-        document.querySelector("#browseView").classList.add("hidePage");
-        document.querySelector("#singleProductView").classList.add("hidePage");
-        document.querySelector("#shoppingCartView").classList.add("hidePage");
-        document.querySelector("#aboutUsView").classList.add("hidePage");
+        document.querySelector("#homeView").style.display = "none";
+        document.querySelector("#singleProductView").style.display = "none";
+        document.querySelector("#shoppingCartView").style.display = "none";
+        document.querySelector("#aboutUsView").style.display = "none";
     }
 
     // ------------------------------------------setup functions ------------
     function headerSetup(){
 
+        //Load to browse view if user presses enter in search bar
         document.querySelector("#searchBarBox").addEventListener("keydown", (e)=>{
             if(e.key == "Enter"){
                 let searchInput = searchBar.value;
@@ -82,13 +85,16 @@ document.addEventListener("DOMContentLoaded", (e)=>{
                 
                 // checks the length (minus whiteSpace) 
                 if(searchInput.replace(/\s+/g, '').length >2){
-                    
+                    clearFilterList();
+                    filterList.name = searchInput;
+                    loadBrowseView();
                 } else{
                     alert("please input a prompt with more than three regular characters");
                 }
             }
         });
 
+        // load to browse view if user clicks the search button in search bar
         document.querySelector("#searchButton").addEventListener("click", (e)=>{
             let searchInput = searchBar.value;
 
@@ -96,16 +102,20 @@ document.addEventListener("DOMContentLoaded", (e)=>{
                 
             // checks the length (minus whiteSpace) 
             if(searchInput.replace(/\s+/g, '').length >2){
-                
+                clearFilterList();
+                filterList.name = searchInput;
+                loadBrowseView();
             } else{
                 alert("please input a prompt with more than three regular characters");
             }
         });
 
+        // load to checkout section if user clicks the shopping cart box
         document.querySelector("#shoppingCartBox").addEventListener("click", (e)=>{
 
         });
 
+        // load to about us section if user clicks the about us box
         document.querySelector("#aboutUsBox").addEventListener("click", (e)=>{
 
         });
@@ -113,6 +123,20 @@ document.addEventListener("DOMContentLoaded", (e)=>{
     }
 
     function homeViewSetup(){
+
+        //load browse view with the filter of gender being male
+        document.querySelector("#menSuper").addEventListener("click",(e)=>{
+            clearFilterList();
+            filterList.gender = "mens";
+            loadBrowseView();
+        });
+
+        //load browse view with the filter of gender being female
+        document.querySelector("#womenSuper").addEventListener("click",(e)=>{
+            clearFilterList();
+            filterList.gender = "womens";
+            loadBrowseView();
+        });
 
     }
 
@@ -143,9 +167,34 @@ document.addEventListener("DOMContentLoaded", (e)=>{
 
     }
 
+    // simply just resets the filter list
+    function clearFilterList(){
+        filterList = {
+            "name":"N/A",
+            "gender":"N/A",
+            "lowPrice":0,
+            "highPrice":"N/A",
+            "category":"N/A",
+            "sizeXS":"N/A",
+            "sizeS":"N/A",
+            "sizeXS":"N/A",
+            "sizeM":"N/A",
+            "sizeL":"N/A",
+            "sizeXL":"N/A",
+            "sortBy":"N/A"
+        };
+    }
+
+    /**
+     * this is toggles the page view when the user switches 
+     * from one sub-page to another sub-page.
+     * 
+     * @param {*} pageOn the page to be toggled on
+     * @param {*} pageOff the previous page to be toggled off
+     */
     function togglePageView(pageOn, pageOff){
-        document.querySelector("#"+pageOff).classlist.add("hidePage");
-        document.querySelector("#"+pageOn).classlist.remove("hidePage");
+        document.querySelector("#"+pageOff).style.display = "none";
+        document.querySelector("#"+pageOn).style.display = "flex";
     }
 
 });
